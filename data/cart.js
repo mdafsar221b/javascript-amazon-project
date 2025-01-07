738625e35991ec0268ac5fdb1ed545d67460e537
Md Afsar <afsarsayyyed@gmail.com>
2025-01-07 23:55:11 +0530

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,22 @@
-export let cart= [{
- productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
- quantity:2
+export let cart= JSON.parse(localStorage.getItem('cart'));
 
-},{
- productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
- quantity:1  
-
+// Setting default value if cart is empty
+if(!cart){
+  cart = [{
+   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+   quantity:2
+  
+  },{
+   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+   quantity:1  
+  
+  }];
 }
 
-];
+function saveTostorage(){
+localStorage.setItem('cart',JSON.stringify(cart));
+
+}
 
 export function addToCart(productId){
 
@@ -32,7 +40,9 @@ export function addToCart(productId){
     
       });
     }
+    saveTostorage();
   }
+
 
   export function removeFromCart(productId){
 const newCart=[];
@@ -43,4 +53,5 @@ cart.forEach((cartItem)=>{
 
 });
  cart=newCart;
+ saveTostorage();
 }
