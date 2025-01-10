b65ab71817aa2eca06c46e39f4d90f06067c223e
Md Afsar <afsarsayyyed@gmail.com>
2025-01-10 21:35:36 +0530

    created test suite for formatCurrency

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,16 +1,24 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 //Automatic test
+
+console.log('test suite: formatCurrency');
+
+console.log('Convert Cents into dollar');
 if(formatCurrency(2095)=== '20.95'){
     console.log('Passed')
 } else {
     console.log('failed')
 }
 
+ console.log('Works with zero');
+
 if(formatCurrency(0)=== '0.00'){
     console.log('Passed')
 } else {
     console.log('failed')
 }
+
+console.log('rounds up to the nearest cents');
 
 if(formatCurrency(2000.5)=== '20.01'){
     console.log('Passed')
