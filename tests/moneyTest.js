02307ed44679a12c4778d90112e958a1ab3a9eb5
Md Afsar <afsarsayyyed@gmail.com>
2025-01-10 21:28:52 +0530

    Added Test Cases for FormatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,24 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+//Automatic test
+if(formatCurrency(2095)=== '20.95'){
+    console.log('Passed')
+} else {
+    console.log('failed')
+}
+
+if(formatCurrency(0)=== '0.00'){
+    console.log('Passed')
+} else {
+    console.log('failed')
+}
+
+if(formatCurrency(2000.5)=== '20.01'){
+    console.log('Passed')
+} else {
+    console.log('failed')
+}
+if(formatCurrency(2000.4)=== '20.00'){
+    console.log('Passed')
+} else {
+    console.log('failed')
+}
