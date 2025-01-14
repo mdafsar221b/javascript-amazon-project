82f11746b6bab5135c32a40db26a61df497b4213
Md Afsar <afsarsayyyed@gmail.com>
2025-01-14 15:17:32 +0530

    make localStorageKey private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,16 +1,15 @@
 class Cart{
- 
     cartItems;
-    localStoragekey;
+    #localStoragekey;
 
     constructor(localStoragekey){
       
-    this.localStoragekey=localStoragekey;
-    this.saveTostorage();
+    this.#localStoragekey=localStoragekey;
+    this.loadFromStorage();
     }
 
     loadFromStorage(){
-        this.cartItems= JSON.parse(localStorage.getItem(this.localStoragekey));
+        this.cartItems= JSON.parse(localStorage.getItem(this.#localStoragekey));
       
         // Setting default value if cart is empty
         if(!this.cartItems){
@@ -19,7 +18,7 @@ class Cart{
            quantity:2,
            deliveryOptionId:'1'
           
-          },{
+          }, {
            productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
            quantity:1,
            deliveryOptionId:'2'
@@ -31,7 +30,7 @@ class Cart{
 
        
   saveTostorage(){
-    localStorage.setItem(this.localStoragekey,JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStoragekey,JSON.stringify(this.cartItems));
     
     }
 
